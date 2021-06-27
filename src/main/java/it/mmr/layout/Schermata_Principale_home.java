8b2df5fd836a96bad8500ef70e741e4f43a3d23f
corrado <corrado.100@libero.it>
2021-06-27 14:39:38 +0200

    aggiunta link per instagram facebook e sito web

--- src/main/java/it/mmr/layout/Schermata_Principale_home.java
+++ src/main/java/it/mmr/layout/Schermata_Principale_home.java
@@ -11,6 +11,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.net.URI;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -22,6 +23,10 @@ public class Schermata_Principale_home extends JFrame implements ActionListener 
     public static int i = 3;
     public static JLayeredPane contenitore;
     public static String warning;
+    JButton sito;
+
+    public JButton instagram, facebook;
+
 
     public Schermata_Principale_home() throws SQLException {
         super("Home");
@@ -29,7 +34,25 @@ public class Schermata_Principale_home extends JFrame implements ActionListener 
         ImageIcon logo_mmr = new ImageIcon("src/main/java/images/mmr_logo.jpg");
 
         BufferedImage resized_blocco_note = Creazione_immagini.creazioneImmagini("src/main/java/images/AVVERTENZE.png", 500, 500);
+        BufferedImage resized_instagram = Creazione_immagini.creazioneImmagini("src/main/java/images/instagram.png", 90, 90);
+        BufferedImage resized_facebook = Creazione_immagini.creazioneImmagini("src/main/java/images/facebook.png", 90, 90);
         BufferedImage resized_danger = Creazione_immagini.creazioneImmagini("src/main/java/images/danger.png", 60, 75);
+        BufferedImage resized_gokart = Creazione_immagini.creazioneImmagini("src/main/java/images/gokart.png", 90, 90);
+
+        sito = new JButton("Portami al sito web");
+        sito.setForeground(new Color(0x0EAAE7));
+        sito.addActionListener(this);
+        sito.setBorder(BorderFactory.createEmptyBorder());
+        sito.setContentAreaFilled(false);
+        sito.setFont(new Font("MONACO", Font.ITALIC, 20));
+
+        JLabel pannello_gokart = new JLabel(new ImageIcon(resized_gokart));
+        pannello_gokart.setVisible(true);
+        pannello_gokart.setBounds(60, 620, 450, 90);
+
+        JPanel pannello_sito = new JPanel(new GridBagLayout());
+        pannello_sito.add(sito);
+        pannello_sito.setBounds(-30, 620, 280, 90);
 
         JTabbedPane divisioni = new JTabbedPane(JTabbedPane.LEFT);
 
@@ -48,11 +71,7 @@ public class Schermata_Principale_home extends JFrame implements ActionListener 
 
         JPanel panello_cambio_password = new JPanel();
         panello_cambio_password.add(cambia_password);
-        if (Login_iniziale.root) {
-            panello_cambio_password.setBounds(80, 900, 200, 30);
-        } else {
-            panello_cambio_password.setBounds(80, 970, 200, 30);
-        }
+        panello_cambio_password.setBounds(60, 310, 200, 30);
 
         JLabel picLabel = new JLabel(new ImageIcon(resized_blocco_note));
         JPanel pannelo_blocconote = new JPanel();
@@ -60,23 +79,43 @@ public class Schermata_Principale_home extends JFrame implements ActionListener 
         JLabel picLabel1 = new JLabel(new ImageIcon(resized_danger));
         JPanel panello_danger = new JPanel();
 
+        instagram = new JButton(new ImageIcon(resized_instagram));
+        instagram.addActionListener(this);
+        instagram.setContentAreaFilled(false);
+        instagram.setBorder(BorderFactory.createEmptyBorder());
+
+        instagram.setOpaque(false);
+
+        JPanel pannello_instagram = new JPanel();
+        pannello_instagram.add(instagram);
+        pannello_instagram.setBounds(40, 200, 90, 90);
+
+        facebook = new JButton(new ImageIcon(resized_facebook));
+        facebook.addActionListener(this);
+        facebook.setBorder(BorderFactory.createEmptyBorder());
+
+        JPanel pannello_facebook = new JPanel();
+        pannello_facebook.setOpaque(true);
+        pannello_facebook.add(facebook);
+        pannello_facebook.setBounds(160, 200, 150, 150);
+
         panello_danger.add(picLabel1);
         panello_danger.setBounds(25, 950, 65, 80);
-
-        //JLabel albicocca=new JLabel(new ImageIcon("src/main/java/images/albicocca.gif"));
-        //JPanel panello_peach=new JPanel();
-        //panello_peach.add(albicocca);
-        //a.add(panello_peach,5,0);
-        //panello_peach.setBounds(0,50,200,160);
 
         pannelo_blocconote.setBackground(new Color(237, 237, 237));
         pannelo_blocconote.add(picLabel);
         pannelo_blocconote.setBounds(20, 330, 300, 300);
+
         divisioni.addTab("", logo_mmr, PannelloTotale.pannelloTotale());
         divisioni.setSize(1920, 1080);
         contenitore.setBounds(0, 0, 1920, 1080);
         contenitore.add(divisioni, 0, 0);
         contenitore.add(pannelo_blocconote, 1, 1);
+        contenitore.add(pannello_gokart,1,1);
+        contenitore.add(pannello_instagram, 1,1);
+        contenitore.add(pannello_facebook,1,1);
+        contenitore.add(pannello_sito,1,1);
+
         if (Login_iniziale.root) {
             contenitore.add(panello_avvertenza, 2, 2);
             contenitore.add(panello_danger, 2, 1);
@@ -100,6 +139,24 @@ public class Schermata_Principale_home extends JFrame implements ActionListener 
         }
         if (e.getSource() == cambia_password) {
             new cambio_password();
+        }
+
+        if(e.getSource() == facebook){
+            try {Desktop.getDesktop().browse(new URI("https://www.facebook.com/MMR.Modena/"));
+            } catch (Exception e2)
+            {JOptionPane.showMessageDialog(null, e2);}
+        }
+        if(e.getSource() == instagram){
+            try {Desktop.getDesktop().browse(new URI("https://www.instagram.com/mmr_modena/"));
+            } catch (Exception e2)
+            {JOptionPane.showMessageDialog(null,e2);}
+        }
+
+        if(e.getSource() == sito){
+            try {Desktop.getDesktop().browse(new URI("https://en.moremodenaracing.it/"));
+            } catch (Exception e2)
+            {JOptionPane.showMessageDialog(null,e2);}
+
         }
     }
 
